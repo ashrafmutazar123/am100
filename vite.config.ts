ee2a2c8a5cf27237bc1fc00a136b4dbf8ec7ba69
unknown <saifakmal.work@gmail.com>
2025-09-17 14:48:42 +0800

    Setup GitHub Pages deploy with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/am100/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: "/am100/",   // 👈 IMPORTANT for GitHub Pages
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
